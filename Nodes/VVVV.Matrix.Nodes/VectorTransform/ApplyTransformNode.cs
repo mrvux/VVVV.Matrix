88e0b3ed2838e3edaa2ce93030f156eccf47c27d
Julien Vulliet <julien@mrvux.com>
2013-12-30 20:21:04 +0100

    Stubs for Apply Transform

---
+++ Nodes/VVVV.Matrix.Nodes/VectorTransform/ApplyTransformNode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVVV.DataTypes;
+using VVVV.Hosting.IO.Pointers;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.Nodes
+{
+    [PluginInfo(Name="ApplyTransform",Category="Transform")]
+    public class ApplyTransformNode : IPluginEvaluate
+    {
+        [Input("Transform")]
+        protected MatrixInput TransformIn;
+
+        [Input("X UnTransformed")]
+        protected FastValueInput XIn;
+
+        [Input("Y UnTransformed")]
+        protected FastValueInput YIn;
+
+        [Input("Z UnTransformed")]
+        protected FastValueInput ZIn;
+
+        [Input("Threaded", IsSingle=true)]
+        protected ISpread<bool> Threaded;
+
+        [Input("X Transformed")]
+        protected ValueOutput XOut;
+
+        [Input("Y Transformed")]
+        protected ValueOutput YOut;
+
+        [Input("Z Transformed")]
+        protected ValueOutput ZOut;
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (this.TransformIn.IsConnected)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ Nodes/VVVV.Matrix.Nodes/VectorTransform/ApplyTransformVectorNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVVV.DataTypes;
+using VVVV.Hosting.IO.Pointers;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.Nodes
+{
+    [PluginInfo(Name="ApplyTransform",Category="Transform", Version="Vector")]
+    public class ApplyTransformVectorNode : IPluginEvaluate
+    {
+        [Input("Transform")]
+        protected MatrixInput TransformIn;
+
+        [Input("XYZ UnTransformed")]
+        protected FastValueInput XYZIn;
+
+        [Input("Threaded", IsSingle=true)]
+        protected ISpread<bool> Threaded;
+
+        [Input("XYZ Transformed")]
+        protected ValueOutput XYZOut;
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (this.TransformIn.IsConnected)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
