c1bc09b776e25ff1c245541bec54a4fc2ba5d554
Julien Vulliet <julien@mrvux.com>
2013-12-30 20:46:28 +0100

    Transform 2d stubs

---
+++ Nodes/VVVV.Matrix.Nodes/2d/Transform2DNode - Copy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVVV.DataTypes;
+using VVVV.Hosting.IO.Pointers;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.Nodes
+{
+    [PluginInfo(Name = "Transform", Category = "2d", Version="")]
+    public class Transform2dNode : IPluginEvaluate
+    {
+        [Input("Transform In")]
+        protected MatrixInput TransformIn;
+
+        [Input("Translate X")]
+        protected FastValueInput TranslateX;
+
+        [Input("Translate Y")]
+        protected FastValueInput TranslateY;
+
+        [Input("Scale X", DefaultValue=1)]
+        protected FastValueInput ScaleX;
+
+        [Input("Scale Y", DefaultValue = 1)]
+        protected FastValueInput ScaleY;
+
+        [Input("Rotate")]
+        protected FastValueInput Rotate;
+
+        [Input("Center X")]
+        protected FastValueInput CenterX;
+
+        [Input("Center Y")]
+        protected FastValueInput CenterY;
+
+        [Input("Threaded", IsSingle = true)]
+        protected ISpread<bool> Threaded;
+
+        [Input("Transform Out")]
+        protected MatrixOutput TransformOut;
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (this.TransformIn.IsConnected)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ Nodes/VVVV.Matrix.Nodes/2d/Transform2DVectorNode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVVV.DataTypes;
+using VVVV.Hosting.IO.Pointers;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.Nodes
+{
+    [PluginInfo(Name = "Transform", Category = "2d", Version="Vector")]
+    public class Transform2dVectorNode : IPluginEvaluate
+    {
+        [Input("Transform In")]
+        protected MatrixInput TransformIn;
+
+        [Input("Translate XY")]
+        protected FastValueInput Position;
+
+        [Input("Scale XY", DefaultValues= new double[] {1,1})]
+        protected FastValueInput Scale;
+
+        [Input("Rotate")]
+        protected FastValueInput Rotate;
+
+        [Input("Center XY")]
+        protected FastValueInput Center;
+
+        [Input("Threaded", IsSingle = true)]
+        protected ISpread<bool> Threaded;
+
+        [Input("Transform Out")]
+        protected MatrixOutput TransformOut;
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (this.TransformIn.IsConnected)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
